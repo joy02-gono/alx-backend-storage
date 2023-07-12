034e17a5d88c804ccd9e1a78a5497baf03fb3e59
joy02-gono <rejoicegono@gmail.com>
2023-07-12 03:10:32 -0700

    1-country_users.sql

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,7 +1,8 @@
--- script that creates a table users with follwing fields
--- id, email, name
-CREATE TABLE IF NOT EXISTS users (
+-- Script that creates a table users following these requirements:
+-- id, email, name, country(enumeration of US, CO and TN)
+CREATE TABLE IF NOT EXISTS users(
 	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	email varchar(255) NOT NULL UNIQUE,
-	name varchar(255)
-)
+	name varchar(255),
+	country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
+);
